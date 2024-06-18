00b311111d6bb0d1757cfe0d89b8d0e3c7c30e5e
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-06-18 13:45:48 -0300

    Adding night mode input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
-const inputCheked = document.querySelector('#modo-noturno');
+const buttonClick = document.querySelector('#modo-noturno');
 const elemento = document.querySelector('body');
+const icons = document.querySelector('#icons');
 
-inputCheked.addEventListener('click', () => {
-    const modo = inputCheked.checked ? 'dark': 'light';
-    elemento.setAttribute('data-bs-theme', modo);
+buttonClick.addEventListener('click', () => {
+    const modo = buttonClick.checked ? 'dark' : 'light';
+    if(modo === 'dark'){
+        elemento.setAttribute('data-bs-theme', modo);
+        icons.classList.add('bi-sun');
+        icons.style ='color: #000';
+    }else if(modo == 'light') {
+        elemento.setAttribute('data-bs-theme', modo);
+        icons.classList.add('bi-moon-stars-fill');
+       
+    }
 })
