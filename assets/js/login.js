e560be711e00381cdc5ebf73730324a47a6fbcf2
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-06-19 15:07:24 -0300

    Adcionando o movimento do painel

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,8 +2,16 @@ const btnLogin = document.querySelector('#login');
 const content = document.querySelector('.container');
 const firstContentOne = document.querySelector('.first-content1');
 const secondContentTruw = document.querySelector('.second-content2');
+const cadastroBtn = document.querySelector('.btn-cadastro');
 
 btnLogin.addEventListener('click', () => {
     firstContentOne.style.display='none';
     secondContentTruw.style.display='inline-flex';
+    firstContentOne.classList.add('movimento');
 });
+
+cadastroBtn.addEventListener('click', () => {
+    firstContentOne.style.display='inline-flex';
+    secondContentTruw.style.display='none';
+    
+})
