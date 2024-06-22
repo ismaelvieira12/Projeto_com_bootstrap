8fd292ebb31d512ec8f171b5a1e17b559f0897b8
Ismael Nasciemnto <117104139+ismaelvieira12@users.noreply.github.com>
2024-06-22 14:13:18 -0300

    Update login.js adicionando a função de ver passworrd

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,6 +3,12 @@ const cadastroBtn = document.querySelector('#sinUp');
 const body = document.querySelector('body');
 const firstContentOne = document.querySelector('.first-content1');
 const secondContentTruw = document.querySelector('.second-content2');
+const inputCheck = document.querySelector('.eye-password');
+const inputCheck2 = document.querySelector('#eye-password2');
+const verPassword = document.querySelector('.eye');
+const verPassword2 = document.querySelector('#eye');
+
+
 
 btnLogin.addEventListener('click', () => {
     secondContentTruw.style.display='inline-flex';
@@ -15,4 +21,29 @@ cadastroBtn.addEventListener('click', () => {
     firstContentOne.style.display='inline-flex';
     secondContentTruw.style.display='none';
     body.className= "sin-up-js"
+})
+
+inputCheck.addEventListener('click', () => {
+    const inputPassword = document.querySelector('.password');
+    
+    if(inputCheck.checked == true){
+        verPassword.classList.add('fa-eye');
+        inputPassword.setAttribute('type', 'text');
+    }
+    else if (inputCheck.checked == false ){
+        verPassword.classList.remove('fa-eye');
+        inputPassword.setAttribute('type', 'password');
+    }
+})
+
+inputCheck2.addEventListener('click', () => {
+    const inputPassword = document.querySelector('#Password2');
+    if (inputCheck2.checked == true){
+        verPassword2.classList.add('fa-eye');
+        inputPassword.setAttribute('type', 'text');
+    }
+    else if (inputCheck2.checked == false ){
+        verPassword2.classList.remove('fa-eye');
+        inputPassword.setAttribute('type', 'password');
+    }
 })
